a8da45230680a01e737b9680daa9e384d8099ee2
poetahto <walls.dk@gmail.com>
2023-12-14 20:56:07 -0800

    temp removed audio window from main, since its unstable

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,16 @@
 
 int main() {
     Platform::init();
-    MicReader::init();
 
     while (!Platform::wantsToQuit()) {
         Platform::handleEvents();
         Platform::Renderer::startFrame();
 
-        MicReader::showDebugWindow();
         ImGui::ShowDemoWindow();
 
         Platform::Renderer::endFrame();
     }
 
-    MicReader::free();
     Platform::free();
     return 0;
 }
